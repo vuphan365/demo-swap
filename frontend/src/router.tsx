f360d979072f12cba01eeeed754ffe9711c9f6af
vuphan365 <vuphan365@gmail.com>
2023-06-29 22:43:06 +0700

    fix: Fix routes issue and change page name

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,17 +1,27 @@
 import {
   createBrowserRouter,
+  useRouteError
 } from "react-router-dom";
 import App from 'views/App'
 import Swap from 'views/Swap'
 
+
+function ErrorBoundary() {
+  let error = useRouteError();
+  console.error(error);
+  return <div>I'm an bug, the developer is looking for me</div>;
+}
+
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/demo-swap",
     element: <App />,
+    errorElement: (<ErrorBoundary />),
     children: [{
       index: true,
       element: <Swap />
-    }]
+    }],
+
   }
 ])
 
