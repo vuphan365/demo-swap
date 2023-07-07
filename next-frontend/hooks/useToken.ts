1fbac674aa89735e6ba7560370b988e979dbbd1b
vuphan365 <vuphan365@gmail.com>
2023-07-07 21:38:23 +0700

    feat: Create new nextjs app for frontend

---
+++ next-frontend/hooks/useToken.ts
@@ -0,0 +1,31 @@
+import { useCallback } from 'react'
+import { Token } from 'types/token'
+import { Token as SDKToken } from '@uniswap/sdk-core'
+import { useWallet } from './useWallet'
+import {
+  ChainId,
+} from '@uniswap/smart-order-router'
+
+export function useToken() {
+  const { getWalletAddress } = useWallet()
+
+  const getLatestTokenBalance = useCallback(async (token: Token) => {
+    if (!token) return
+    const address = await getWalletAddress()
+    const balance = await token?.contract?.balanceOf(address)
+    const newToken = new SDKToken(
+      ChainId.GÖRLI,
+      token.address,
+      18,
+      token.symbol,
+      token.name
+    ) as Token
+    newToken.balance = balance;
+    newToken.contract = token?.contract
+    return newToken
+  }, [getWalletAddress])
+
+  return {
+    getLatestTokenBalance
+  }
+}
