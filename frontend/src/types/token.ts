62c31212f89c2f593b4ab61327953207992589a1
vuphan365 <vuphan365@gmail.com>
2023-07-08 20:57:45 +0700

    feat: 🎸 Replace frontend with next-frontend

--- frontend/src/types/token.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'ethers'
-import { Token as SwapToken } from '@uniswap/sdk-core'
-
-export interface Token extends SwapToken {
-  contract?: ethers.Contract,
-  balance?: ethers.BigNumber,
-}
